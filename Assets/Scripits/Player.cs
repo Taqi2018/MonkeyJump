409e0e6d52ac4137513b4fa34ff34bacf146612e
AbdullahKhan1357 <abdkhan13575@gmail.com>
2023-12-06 04:01:45 +0500

    Jump Animation

--- Assets/Scripits/Player.cs
+++ Assets/Scripits/Player.cs
@@ -4,62 +4,64 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public Transform player;
-    public float xDistance;
-    public float speed;
+     public Transform player;
+     public float xDistance;
+     public float speed;
 
-    Vector3 dirToJump;
-    private float previousTime;
-    public float timeDelayForJump;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-      
-        dirToJump = Vector3.right;
-        previousTime = 0;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            float spacePressedTime = Time.time;
-            float timeDelayBetweenJump = spacePressedTime - previousTime;
-           
-            if (timeDelayBetweenJump >timeDelayForJump)
-            {
-                Vector3 targetPosition = player.position + dirToJump * xDistance;
-                StartCoroutine(MovePlayerToPosition(targetPosition, speed));
-                dirToJump = -dirToJump;
-                transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
-
-                previousTime = spacePressedTime;
-
-            }
-        
-           
-
-        }
-    }
-
-    IEnumerator MovePlayerToPosition(Vector3 targetPosition, float moveSpeed)
-    {
-        float elapsedTime = 0f;
-        Vector3 startingPos = player.position;
-
-        while (elapsedTime < 1f)
-        {
-
-            player.position = Vector3.Lerp(startingPos, targetPosition, elapsedTime);
-            Debug.Log(player.position);
-            elapsedTime += Time.deltaTime * moveSpeed;
-            yield return null;
+     Vector3 dirToJump;
+     private float previousTime;
+     public float timeDelayForJump;
 
 
-        }
 
-     //   player.position = targetPosition;
-    }
+     // Start is called before the first frame update
+     void Start()
+     {
+
+          dirToJump = Vector3.right;
+          previousTime = 0;
+     }
+
+     // Update is called once per frame
+     void Update()
+     {
+          if (Input.GetKeyDown(KeyCode.Space))
+          {
+               float spacePressedTime = Time.time;
+               float timeDelayBetweenJump = spacePressedTime - previousTime;
+
+               if (timeDelayBetweenJump > timeDelayForJump)
+               {
+                    Vector3 targetPosition = player.position + dirToJump * xDistance;
+                    StartCoroutine(MovePlayerToPosition(targetPosition, speed));
+                    dirToJump = -dirToJump;
+                    transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+
+                    previousTime = spacePressedTime;
+
+               }
+
+
+
+          }
+     }
+
+     IEnumerator MovePlayerToPosition(Vector3 targetPosition, float moveSpeed)
+     {
+          float elapsedTime = 0f;
+          Vector3 startingPos = player.position;
+
+          while (elapsedTime < 1f)
+          {
+
+               player.position = Vector3.Lerp(startingPos, targetPosition, elapsedTime);
+               Debug.Log(player.position);
+               elapsedTime += Time.deltaTime * moveSpeed;
+               yield return null;
+
+
+          }
+
+          //   player.position = targetPosition;
+     }
 }
